858f034e82d8c30aaa69f05b386212cd33bb91e8
jmkim <isugar150@gmail.com>
2025-11-20 19:20:58 +0900

    🔥

---
+++ backend/src/test/java/com/namejm/query_bot/QueryBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namejm.query_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
